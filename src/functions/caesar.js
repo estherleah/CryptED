519690183d7d948f318edef98c22f2574653429f
estherleah <estherleahmorrison@gmail.com>
2017-07-18 12:06:52 +0100

    Add caesar cipher function

---
+++ src/functions/caesar.js
@@ -0,0 +1,39 @@
+function caesar(plaintext) {
+    // output
+    var ciphertext = "Ciphertext: ";
+
+    // pick random number between 1 and 25 as the shift value
+    var shift = Math.floor((Math.random() * 25) + 1);
+
+    // go through plaintext one character at a time
+    for (var i = 0; i < plaintext.length; i++) {
+        var c = plaintext[i];
+        // check if it is a letter
+        if (c.match(/[a-z]/i)) {
+            // get unicode value
+            var unicode = plaintext.charCodeAt(i);
+            // uppercase
+            if ((unicode >= 65) && (unicode <= 90)) {
+                c = String.fromCharCode(((unicode - 65 + shift) % 26) + 65);
+            }
+            // lowercase
+            else if ((unicode >= 97) && (unicode <= 122)) {
+                c = String.fromCharCode(((unicode - 97 + shift) % 26) + 97);
+            }
+        }
+        // append to ciphertext
+        ciphertext += c;
+    }
+    return ciphertext;
+}
+
+// var plaintext = "Hello World!";
+
+// console.log(caesar(plaintext));
+// var response = prompt("Plaintext:");
+
+// if (response.toLowerCase() == plaintext.toLowerCase()) {
+//     console.log(plaintext + " is correct!");
+// } else {
+//     console.log("Please try again");
+// }
