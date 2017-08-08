6a2cb465bb770e98f234c69e94416ff9691f882f
estherleah <estherleahmorrison@gmail.com>
2017-08-08 02:18:31 +0100

    Tidying up

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -90,7 +90,7 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 scores: action.payload,
-            }
+            };
             
         default:
             return state;
